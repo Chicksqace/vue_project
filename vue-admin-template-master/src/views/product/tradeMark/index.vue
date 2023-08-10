c8a6eb427a6dfccba5ee49ea69cba5a2535ae84d
陈攀宇 <2965390478@qq.com>
2023-08-10 22:41:38 +0800

    品牌列表展示完成

--- vue-admin-template-master/src/views/product/tradeMark/index.vue
+++ vue-admin-template-master/src/views/product/tradeMark/index.vue
@@ -3,14 +3,21 @@
     <!-- 按钮 -->
     <el-button type="primary" icon="el-icon-plus" style="margin: 10px 0px;">添加</el-button>
     <!-- 表格组件 -->
-    <el-table style="width: 100%;" border>
-      <el-table-column  prop="prop" label="序号" width="80px">
+    <el-table style="width: 100%;" border :data="list">
+      <el-table-column type="index" prop="prop" label="序号" width="80px">
       </el-table-column>
-      <el-table-column  prop="prop" label="品牌名称" width="width">
+      <el-table-column prop="tmName" label="品牌名称" width="width">
       </el-table-column>
-      <el-table-column  prop="prop" label="品牌logo" width="width">
+      <el-table-column  prop="logoUrl" label="品牌logo" width="width">
+        <template slot-scope="{row,$index}">
+          <img :src="row.logoUrl" alt="" style="width: 100px;height: 100px;">
+        </template>
       </el-table-column>
       <el-table-column  prop="prop" label="操作" width="width">
+        <template slot-scope="{row,$index}">
+          <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
       </el-table-column>
     </el-table>
     <!-- 分页器 -->
@@ -29,13 +36,15 @@
 
      -->
     <el-pagination
-    style="margin-top: 20px;text-align: center;"
-     :current-page="6"
+    style="margin-top: 20px;textAlign: center;"
+     :current-page="page"
       :page-sizes="[3,5,10]"
       :page-size="3"
-      :pager-count="9"
+      :pager-count="7"
       layout="prev, pager, next, jumper,->,sizes,total"
-      :total="99" background>
+      @size-change="sizeChange"
+      @current-change="getPageList"
+      :total="total" background>
       >
     </el-pagination>
     
@@ -50,6 +59,10 @@ export default {
       page:1,
       // 当前页数展示多少条数据
       limit:3,
+      // 总共数据条数
+      total:0,
+      // 列表展示的数据
+      list:[]
     }
   },
   mounted(){
@@ -57,12 +70,20 @@ export default {
     this.getPageList()
   },
   methods:{
-    async getPageList(){
-      console.log(1);
+    async getPageList(pager=1){
+      this.page=pager
       const {page,limit}=this;
       // 这个写接口需要传参
       let result=await this.$Api.trademark.reqTradeMarkList(page,limit);
-      console.log(result);
+      if(result.code==200){
+        this.total=result.data.total;
+        this.list=result.data.records;
+      }
+    },
+    // 当分页器某一页需要战术数据条数发生变化的时候触发
+    sizeChange(limit){
+      this.limit=limit
+      this.getPageList()
     }
   }
 }
