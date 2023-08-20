758a49ae2be64f770e5e8ff221c577c383447333
陈攀宇 <2965390478@qq.com>
2023-08-20 21:48:53 +0800

    三级联动完成

--- vue-admin-template-master/src/api/product/attr.js
+++ vue-admin-template-master/src/api/product/attr.js
@@ -4,10 +4,10 @@ import request from '@/utils/request';
 export const reqCategory1List=()=>request({url:'/dev1-api/admin/product/getCategory1',method:'get'})
 
 // 获取二级
-export const reqCategory2List=()=>request({url:`/dev1-api/admin/product/getCategory2/${category1Id}`,method:'get'})
+export const reqCategory2List=(category1Id)=>request({url:`/dev1-api/admin/product/getCategory2/${category1Id}`,method:'get'})
 // export function reqCategory2List(){return request({url:'/admin/product/getCategory2',method:'get'})}
 
 
 // 获取三级
-export const reqCategory3List=()=>request({url:`/dev1-api/admin/product/getCategory3/${category2Id}`,method:'get'})
+export const reqCategory3List=(category2Id)=>request({url:`/dev1-api/admin/product/getCategory3/${category2Id}`,method:'get'})
 
--- vue-admin-template-master/src/views/product/Attr/index.vue
+++ vue-admin-template-master/src/views/product/Attr/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-card style="margin: 20px 0px;">
-      <CategorySelect></CategorySelect>
+      <CategorySelect @getCategoryId="getCategoryId"></CategorySelect>
     </el-card>
     <el-card></el-card>
   </div>
@@ -9,7 +9,30 @@
 
 <script>
 export default {
-
+ data() {
+  return {
+   category1Id:'',
+   category2Id:'',
+   category3Id:'' 
+  }
+ },
+ methods:{
+  getCategoryId({categoryId,level}){
+    if(level==1){
+      this.category1Id=categoryId;
+      this.category2Id='',
+      this.category3Id=''
+    }else if(level==2){
+      this.category2Id=categoryId;
+      this.category3Id='';
+    }else{
+      // 代表三级分类的id有了
+      this.category3Id=categoryId
+      // 发请求获取品牌属性
+      this.getAttrList()
+    }
+  }
+ }
 }
 </script>
 
