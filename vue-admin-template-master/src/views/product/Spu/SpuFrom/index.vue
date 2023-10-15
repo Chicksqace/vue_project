515f496861ba2d3544d612a4403c7fdae690c5b8
陈攀宇 <2965390478@qq.com>
2023-10-15 12:08:40 +0800

    SpuForm销售属性的数据展示

--- vue-admin-template-master/src/views/product/Spu/SpuFrom/index.vue
+++ vue-admin-template-master/src/views/product/Spu/SpuFrom/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
-    <el-form ref="form" label-width="80px">
+    {{unSelectSaleAttr}}
+    <el-form ref="form" label-width="80px" :model="spu">
       <el-form-item label="spu名称">
-        <el-input placeholder="spu名称"></el-input>
+        <el-input placeholder="spu名称" value="123" v-model="spu.spuName"></el-input>
       </el-form-item>
       <el-form-item label="品牌">
-        <el-select placeholder="请选择品牌" value="">
-          <el-option label="" value=""></el-option>
+        <el-select placeholder="请选择品牌" v-model="spu.tmId">
+          <el-option :label="tm.Name" :value="tm.id" v-for="(tm, index) in tradeMarkList" :key="tm.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="spu描述">
-        <el-input type="textarea" rows="4" placeholder="描述"></el-input>
+        <el-input type="textarea" rows="4" placeholder="描述" v-model="spu.description"></el-input>
       </el-form-item>
       <el-form-item label="spu图片">
-        <el-upload action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+        <el-upload action="/dev1-api/admin/product/fileUpload" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :file-list="spuImageList">
           <i class="el-icon-plus"></i>
         </el-upload>
         <el-dialog :visible.sync="dialogVisible">
@@ -21,20 +22,35 @@
         </el-dialog>
       </el-form-item>
       <el-form-item label="销售属性">
-        <el-select placeholder="还有三项未选择" value="">
-          <el-option label="label" value="value"></el-option>
+        <el-select :placeholder="`还有${unSelectSaleAttr.length}项未选择`" v-model="attrId">
+          <el-option :label="unselect.name" :value="unselect.id" v-for="(unselect,index) in unSelectSaleAttr" :key="unselect.id"></el-option>
         </el-select>
-        <el-button type="primary" icon="el-icon-plus">添加销售属性</el-button>
-        <el-table style="width: 100%;" border >
-          <el-table-colum type="index" label="序号" width="80px" align="center"></el-table-colum>
-          <el-table-colum type="index" label="属性名" width="width"></el-table-colum>
-          <el-table-colum type="index" label="属性名称列表" width="width" ></el-table-colum>
-          <el-table-colum type="index" label="操作" width="width" ></el-table-colum>
+        <el-button type="primary" icon="el-icon-plus" :disabled="!attrId">添加销售属性</el-button>
+        <el-table border style="width: 100%;" :data="spu.spuSaleAttrList">
+          <el-table-column type="index" label="序号" width="80px" align="center"></el-table-column>
+          <el-table-column prop="saleAttrName" label="属性名" width="width"></el-table-column>
+          <el-table-column prop="prop" label="属性值名称列表" width="width">
+            <template slot-scope="{ row, $index }">
+              <!-- @close="handleClose(tag)" -->
+              <el-tag :key="tag" v-for="tag in row.spuSaleAttrValueList" closable :disable-transitions="false" >
+                {{ tag.saleAttrValueName }}
+              </el-tag>
+              <!-- @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm" -->
+              <el-input class="input-new-tag" v-if="inputVisible" v-model="row.inputValue" ref="saveTagInput" size="small"> </el-input>
+              <!--  @click="showInput" -->
+              <el-button v-else class="button-new-tag" size="small">添加</el-button>
+            </template>
+          </el-table-column>
+          <el-table-column prop="index" label="操作" width="width" >
+            <template slot-scope="{ row, $index }">
+              <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            </template>
+          </el-table-column>
         </el-table>
       </el-form-item>
       <el-form-item>
         <el-button type="primary">保存</el-button>
-        <el-button @click="$emit('changeScene',0)">取消</el-button>
+        <el-button @click="$emit('changeScene', 0)">取消</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -42,51 +58,113 @@
 
 <script>
 export default {
-  name:"",
+  name: 'SpuFrom',
   data() {
-      return {
-        dialogImageUrl: '',
-        dialogVisible: false,
-        spu:{},
-        tradeMarkList:[],
-        spuImageList:[],
-        saleAttrList:[]
-      };
+    return {
+      dialogImageUrl: '',
+      dialogVisible: false,
+      spu: {
+        category3Id: 0,
+        tmId: 0,
+        description: '',
+        spuName: '',
+        spuImageList: [
+          // {
+          //   "id": 0,
+          //   "imgName": "string",
+          //   "imgUrl": "string",
+          //   "spuId": 0
+          // }
+        ],
+
+        spuSaleAttrList: [
+          // {
+          //   baseSaleAttrId: 0,
+          //   id: 0,
+          //   saleAttrName: 'string',
+          //   spuId: 0,
+          //   spuSaleAttrValueList: [
+          //     {
+          //       baseSaleAttrId: 0,
+          //       id: 0,
+          //       isChecked: 'string',
+          //       saleAttrName: 'string',
+          //       saleAttrValueName: 'string',
+          //       spuId: 0
+          //     }
+          //   ]
+          // }
+        ]
+      },
+      tradeMarkList: [],
+      spuImageList: [],
+      saleAttrList: [],
+      attrId:'',//未选择的销售属性的id
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList)
     },
-    methods: {
-      handleRemove(file, fileList) {
-        console.log(file, fileList);
-      },
-      handlePictureCardPreview(file) {
-        this.dialogImageUrl = file.url;
-        this.dialogVisible = true;
-      },
-      // 初始化SpuForm数据
-      async initSpuData(spu){
-        // spu信息
-        let spuResult=await this.$Api.spu.reqSpu(spu.id)
-        if(spuResult.code==200){
-          this.spu=spuResult
-        }
-        // 品牌信息
-        let tradeMarkResult=await this.$Api.spu.reqTradeMarkList()
-        if(tradeMarkResult.code==200){
-          this.tradeMarkList=tradeMarkResult.data
-        }
-        // spu图片
-        let spuImageResult=await this.$Api.spu.reqSpuImageList(spu.id)
-        if(spuImageResult.code==200){
-          this.spuImageList=spuImageResult.data
-        }
-        // 平台全部的销售属性
-        let saleResult=await this.$Api.spu.reqBaseSaleAttrList()
-        if(saleResult.code==200){
-          this.saleAttrList=saleResult.data
-        }
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    // 初始化SpuForm数据
+    async initSpuData(spu) {
+      // spu信息
+      let spuResult = await this.$Api.spu.reqSpu(spu.id)
+      if (spuResult.code == 200) {
+        this.spu = spuResult.data
+      }
+      // 品牌信息
+      let tradeMarkResult = await this.$Api.spu.reqTradeMarkList()
+      if (tradeMarkResult.code == 200) {
+        this.tradeMarkList = tradeMarkResult.data
+      }
+      // spu图片
+      let spuImageResult = await this.$Api.spu.reqSpuImageList(spu.id)
+      if (spuImageResult.code == 200) {
+        let listArr = spuImageResult.data
+        listArr.forEach((item) => {
+          ;(item.name = item.imgName), (item.url = item.imgUrl)
+        })
+        this.spuImageList = listArr
+      }
+      // 平台全部的销售属性
+      let saleResult = await this.$Api.spu.reqBaseSaleAttrList()
+      if (saleResult.code == 200) {
+        this.saleAttrList = saleResult.data
       }
     }
+  },
+  computed:{
+    unSelectSaleAttr(){
+      let result=this.saleAttrList.filter(item=>{
+        return this.spu.spuSaleAttrList.every(item1=>{
+          return item.name!=item1.saleAttrName
+        })
+      })
+      return result;
+    }
+  }
 }
 </script>
 
 <style>
+.el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
 </style>
