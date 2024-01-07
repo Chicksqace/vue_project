13fc849a735f28f7892a51fa9cb630c74e5ee9a1
陈攀宇 <2965390478@qq.com>
2024-01-07 19:06:01 +0800

    1

--- vue-admin-template-master/src/views/dashboard/index.vue
+++ vue-admin-template-master/src/views/dashboard/index.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Card></Card>
+    <Sale></Sale>
   </div>
 </template>
 
 <script>
 import Card from './Card'
+import Sale from './Sale'
 export default {
   components:{
-    Card
+    Card,
+    Sale
   }
 
 }
